25d93eff20e41734b1890484991a217917a07bf6
Olivier Lamy <olamy@apache.org>
2022-03-18 19:45:22 +1000

    quiet
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- src/test/java/org/eclipse/jetty/tck/AppTest.java
+++ src/test/java/org/eclipse/jetty/tck/AppTest.java
@@ -3,13 +3,13 @@ package org.eclipse.jetty.tck;
 import org.junit.jupiter.api.Test;
 import com.sun.ts.tests.servlet.api.jakarta_servlet_http.httpservlet.URLClient;
 
-
+import com.sun.ts.tests.servlet.api.jakarta_servlet_http.httpservletrequest40.Client;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest extends URLClient
+public class AppTest extends Client // URLClient
 {
 
     @Test
