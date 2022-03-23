73e26f6771ee232d7083434b311ce5dcccb1f81f
Olivier Lamy <olamy@apache.org>
2022-03-23 15:50:57 +1000

    declare snapshot repository
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- src/test/java/org/eclipse/jetty/tck/AppTest.java
+++ src/test/java/org/eclipse/jetty/tck/AppTest.java
@@ -1,7 +1,7 @@
 package org.eclipse.jetty.tck;
 
 import org.junit.jupiter.api.Test;
-import com.sun.ts.tests.servlet.api.jakarta_servlet_http.httpservlet.URLClient;
+import com.sun.ts.tests.servlet.api.jakarta_servlet_http.httpservletresponsewrapper.URLClient;
 
 import com.sun.ts.tests.servlet.api.jakarta_servlet_http.httpservletrequest40.Client;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -9,13 +9,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 /**
  * Unit test for simple App.
  */
-public class AppTest extends Client // URLClient
+public class AppTest extends URLClient
 {
 
-    @Test
-    public void shouldAnswerWithTrue() throws Exception
-    {
-        //super.constructorIllegalArgumentExceptionTest();
-    }
 
 }
