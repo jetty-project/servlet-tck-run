6cf687dcdd89075b944b2485620c899f3d902647
Olivier Lamy <olamy@apache.org>
2022-03-24 15:27:54 +1000

    add jsp support
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- src/test/java/org/eclipse/jetty/tck/AppTest.java
+++ src/test/java/org/eclipse/jetty/tck/AppTest.java
@@ -1,7 +1,8 @@
 package org.eclipse.jetty.tck;
 
 import org.junit.jupiter.api.Test;
-import com.sun.ts.tests.servlet.api.jakarta_servlet_http.httpservletresponsewrapper.URLClient;
+//import com.sun.ts.tests.servlet.api.jakarta_servlet_http.httpservletresponsewrapper.URLClient;
+
 
 import com.sun.ts.tests.servlet.api.jakarta_servlet_http.httpservletrequest40.Client;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -9,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 /**
  * Unit test for simple App.
  */
-public class AppTest extends URLClient
+public class AppTest extends com.sun.ts.tests.servlet.spec.welcomefiles.URLClient
 {
 
 
