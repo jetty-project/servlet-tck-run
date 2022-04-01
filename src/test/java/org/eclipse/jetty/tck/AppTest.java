42b43b6c8c49f995fc38d6f69be701d6f806df86
Olivier Lamy <olamy@apache.org>
2022-04-01 18:43:45 +1000

    use ssl certificates
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- src/test/java/org/eclipse/jetty/tck/AppTest.java
+++ src/test/java/org/eclipse/jetty/tck/AppTest.java
@@ -3,7 +3,7 @@ package org.eclipse.jetty.tck;
 /**
  * Unit test for simple App.
  */
-public class AppTest extends  com.sun.ts.tests.servlet.pluggability.fragment.URLClient // com.sun.ts.tests.servlet.api.jakarta_servlet_http.sessioncookieconfig.URLClient // com.sun.ts.tests.servlet.spec.security.annotations.Client
+public class AppTest extends com.sun.ts.tests.servlet.spec.security.clientcertanno.Client // com.sun.ts.tests.servlet.api.jakarta_servlet_http.sessioncookieconfig.URLClient // com.sun.ts.tests.servlet.spec.security.annotations.Client
 {
 
 }
