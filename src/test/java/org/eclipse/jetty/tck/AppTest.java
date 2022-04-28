8ba56563ad156bb1401a3e95f48c77502c89df18
Olivier Lamy <olamy@apache.org>
2022-04-28 16:00:29 +1000

    skip existing certificates:
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- src/test/java/org/eclipse/jetty/tck/AppTest.java
+++ src/test/java/org/eclipse/jetty/tck/AppTest.java
@@ -1,9 +1,16 @@
 package org.eclipse.jetty.tck;
 
+import org.junit.jupiter.api.Test;
+
 /**
  * Unit test for simple App.
  */
-public class AppTest extends com.sun.ts.tests.servlet.spec.serverpush.Client // com.sun.ts.tests.servlet.api.jakarta_servlet_http.sessioncookieconfig.URLClient // com.sun.ts.tests.servlet.spec.security.annotations.Client
+public class AppTest extends  com.sun.ts.tests.servlet.api.jakarta_servlet_http.sessioncookieconfig.URLClient // com.sun.ts.tests.servlet.api.jakarta_servlet_http.sessioncookieconfig.URLClient // com.sun.ts.tests.servlet.spec.security.annotations.Client
 {
+
+//    @Test
+//    public void foo() throws Exception {
+//        super.serverPushInitTest();
+//    }
 
 }
