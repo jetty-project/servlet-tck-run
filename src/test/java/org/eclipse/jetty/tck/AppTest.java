3f00c7175278ded7ec46fb44cc016ae92353305d
Olivier Lamy <olamy@apache.org>
2022-09-04 21:41:30 +1000

    fix some exclusions for slf4j 2.0
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- src/test/java/org/eclipse/jetty/tck/AppTest.java
+++ src/test/java/org/eclipse/jetty/tck/AppTest.java
@@ -1,6 +1,7 @@
 package org.eclipse.jetty.tck;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
 
 /**
  * Unit test for simple App.
@@ -8,9 +9,14 @@ import org.junit.jupiter.api.Test;
 public class AppTest extends  com.sun.ts.tests.servlet.api.jakarta_servlet_http.sessioncookieconfig.URLClient // com.sun.ts.tests.servlet.api.jakarta_servlet_http.sessioncookieconfig.URLClient // com.sun.ts.tests.servlet.spec.security.annotations.Client
 {
 
-//    @Test
-//    public void foo() throws Exception {
-//        super.serverPushInitTest();
-//    }
+    @Test
+    public void foo() throws Exception {
+        //
+    }
+
+    @Test
+    public void bar() throws Exception {
+        LoggerFactory loggerFactory;
+    }
 
 }
