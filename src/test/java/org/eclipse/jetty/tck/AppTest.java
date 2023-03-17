dde03554938c60b877aba51dc20ec90ed25e18a2
Olivier Lamy <olamy@apache.org>
2023-03-17 15:32:20 +1000

    clientcert test
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- src/test/java/org/eclipse/jetty/tck/AppTest.java
+++ src/test/java/org/eclipse/jetty/tck/AppTest.java
@@ -6,17 +6,12 @@ import org.slf4j.LoggerFactory;
 /**
  * Unit test for simple App.
  */
-public class AppTest extends  com.sun.ts.tests.servlet.api.jakarta_servlet_http.sessioncookieconfig.URLClient // com.sun.ts.tests.servlet.api.jakarta_servlet_http.sessioncookieconfig.URLClient // com.sun.ts.tests.servlet.spec.security.annotations.Client
+public class AppTest extends com.sun.ts.tests.servlet.spec.security.clientcert.Client
 {
 
     @Test
     public void foo() throws Exception {
-        //
-    }
-
-    @Test
-    public void bar() throws Exception {
-        LoggerFactory loggerFactory;
+        super.clientCertTest();
     }
 
 }
