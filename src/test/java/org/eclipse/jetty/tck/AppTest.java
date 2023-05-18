36213b7865cd3593b513d3d41b4bce4b25ec1f36
Olivier Lamy <olamy@apache.org>
2023-05-18 15:36:52 +1000

    foo test
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- src/test/java/org/eclipse/jetty/tck/AppTest.java
+++ src/test/java/org/eclipse/jetty/tck/AppTest.java
@@ -6,12 +6,12 @@ import org.slf4j.LoggerFactory;
 /**
  * Unit test for simple App.
  */
-public class AppTest extends com.sun.ts.tests.servlet.spec.security.clientcert.Client
+public class AppTest extends com.sun.ts.tests.servlet.spec.security.secform.Client // com.sun.ts.tests.servlet.api.jakarta_servlet_http.httpsessionx.URLClient
 {
 
     @Test
     public void foo() throws Exception {
-        super.clientCertTest();
+        super.test1();
     }
 
 }
