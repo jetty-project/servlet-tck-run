980c67dea20f3e8e64dd8f77f1df6391457c0bd3
Olivier Lamy <olamy@apache.org>
2023-11-01 12:06:18 +1000

    fix jetty version default
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- src/test/java/org/eclipse/jetty/tck/AppTest.java
+++ src/test/java/org/eclipse/jetty/tck/AppTest.java
@@ -2,18 +2,23 @@ package org.eclipse.jetty.tck;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import servlet.tck.api.jakarta_servlet.dispatchtest.DispatchTests;
 import servlet.tck.api.jakarta_servlet.servletrequest.ServletRequestTests;
+import servlet.tck.api.jakarta_servlet_http.httpsessionx.HttpSessionxTests;
 
 /**
  * Unit test for simple App.
  */
 @Disabled
-public class AppTest extends ServletRequestTests
+public class AppTest extends DispatchTests
+
 {
 
     @Test
     public void foo() throws Exception {
-        super.setCharacterEncodingTest1();
+        System.setProperty("servlet.tck.support.crossContext", "false");
+        //super.invalidateHttpSessionTest();
+        super.negativeDispatchTest9();
     }
 
 }
