d9c9dd3e677acddcfd024ec0ae4546fe2f7cc68f
Olivier Lamy <olamy@apache.org>
2024-06-21 22:07:07 +1000

    test
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- src/test/java/org/eclipse/jetty/tck/AppTest.java
+++ src/test/java/org/eclipse/jetty/tck/AppTest.java
@@ -1,5 +1,7 @@
 package org.eclipse.jetty.tck;
 
+import org.jboss.arquillian.junit5.JUnitJupiterTestClassLifecycleManager;
+import org.jboss.arquillian.test.spi.TestRunnerAdaptor;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import servlet.tck.api.jakarta_servlet.dispatchtest.DispatchTests;
@@ -18,6 +20,7 @@ public class AppTest extends SecFormTests
 
     @Test
     public void foo() throws Exception {
+        JUnitJupiterTestClassLifecycleManager s;
 //        System.setProperty("servlet.tck.support.crossContext", "false");
 //        //super.invalidateHttpSessionTest();
 //        super.flushBufferTest();
